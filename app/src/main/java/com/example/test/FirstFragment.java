300203b25d758e34571c58fcbb4bc6119c0c0aaf
Suraj Kumar <suraj.kumar@rsllearning.com>
2022-08-31 09:02:24 +0530

    communication established

---
+++ app/src/main/java/com/example/test/FirstFragment.java
@@ -0,0 +1,100 @@
+package com.example.test;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FirstFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FirstFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    public static  String key_name="jaga";
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FirstFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FirstFragment newInstance(String param1, String param2) {
+        FirstFragment fragment = new FirstFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+        View v=inflater.inflate(R.layout.fragment_first, container, false);
+
+        Button btn1=v.findViewById(R.id.f1_button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle=new Bundle();
+                EditText ed=v.findViewById(R.id.message_f1);
+                bundle.putString(key_name,ed.getText().toString());
+                getParentFragmentManager().setFragmentResult("dataFrom1",bundle);
+
+                ed.setText("");
+
+            }
+        });
+        getParentFragmentManager().setFragmentResultListener("dataFrom2", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                String s=result.getString(SecondFragment.hariya);
+                TextView tv=v.findViewById(R.id.textView_f1);
+                tv.setText(s);
+
+            }
+        });
+
+
+        
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/test/SecondFragment.java
@@ -0,0 +1,95 @@
+package com.example.test;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SecondFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SecondFragment extends Fragment {
+       private  String s;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    public static  String hariya="empty";
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SecondFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SecondFragment newInstance(String param1, String param2) {
+        SecondFragment fragment = new SecondFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v=inflater.inflate(R.layout.fragment_second, container, false);
+        Button btn2=v.findViewById(R.id.f2_button);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText text2=v.findViewById(R.id.message_f2);
+                Bundle bundle=new Bundle();
+                bundle.putString(hariya,text2.getText().toString());
+                getParentFragmentManager().setFragmentResult("dataFrom2",bundle);
+
+                text2.setText("");
+            }
+        });
+        getParentFragmentManager().setFragmentResultListener("dataFrom1", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                String s=result.getString(FirstFragment.key_name);
+                TextView tv=v.findViewById(R.id.textView_f2);
+                tv.setText(s);
+                
+            }
+        });
+
+
+        return v;
+    }
+}
