986f2c5427e1d9b285d64cd68dee503990cc23eb
Suraj Kumar <suraj.kumar@rsllearning.com>
2022-08-31 09:07:35 +0530

    communication established comment

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.test;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
--- app/src/main/java/com/example/test/SecondFragment.java
+++ app/src/main/java/com/example/test/SecondFragment.java
@@ -1,7 +1,6 @@
 package com.example.test;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentResultListener;
